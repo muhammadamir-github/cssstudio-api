c840657d328e017ba3dbf2c76e37e1fbc79ab789
Amir <86715539+muhammadamir-github@users.noreply.github.com>
2021-09-01 19:12:10 +0530

    .

--- app/Http/Controllers/ThirdParty/GiphyController.php
+++ app/Http/Controllers/ThirdParty/GiphyController.php
@@ -11,7 +11,7 @@ class GiphyController extends Controller
 {
     public function search($query){
         $client = new Guzzle;
-        $response = $client->request('GET','https://api.giphy.com/v1/gifs/search?api_key=ckdUJfg3AXYLXf8xZsVXfyQOUTKR3Mjl&q='.$query.'&limit=50&lang=en&fmt=json');
+        $response = $client->request('GET','https://api.giphy.com/v1/gifs/search?api_key='.env("GIPHY_API_KEY").'&q='.$query.'&limit=50&lang=en&fmt=json');
         $response = $response->getBody()->getContents();
 
         return response()->json($response);
--- app/Http/Controllers/ThirdParty/GoogleFontsController.php
+++ app/Http/Controllers/ThirdParty/GoogleFontsController.php
@@ -10,7 +10,7 @@ class GoogleFontsController extends Controller
 {
     public function all(Request $request){
         $client = new Guzzle();
-        $response = $client->request('GET', 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyBktJP9ry3C1ybMFUWrdZDs9Cj0ANM2Rq0');
+        $response = $client->request('GET', 'https://www.googleapis.com/webfonts/v1/webfonts?key='.env("GOOGLE_API_KEY"));
         $response = $response->getBody()->getContents();
 
         return response()->json($response);
